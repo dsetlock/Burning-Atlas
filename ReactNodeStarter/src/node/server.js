861de4c278f59fc9654fce513cb4df1828819094
daniel.setlock <dsetlock@microsoft.com>
2023-07-07 21:04:30 -0400

    updating comments

--- ReactNodeStarter/src/node/server.js
+++ ReactNodeStarter/src/node/server.js
@@ -10,6 +10,10 @@ app.use(express.static(staticPath));
 // Allows you to set port in the project properties.
 app.set('port', process.env.PORT || 3000);
 
+/**
+ * Starts the server and listens to incoming requests on the specified port.
+ * Logs a message to the console when the server starts listening.
+ */
 var server = app.listen(app.get('port'), function () {
     console.log('listening');
 });
